135d1a4645ac2e6feafda8e0b7f5717b906b05f4
Akzhol <72283384+serikqaliev@users.noreply.github.com>
2022-05-13 22:21:28 +0600

    Update SyntaxInput.js

--- models/Syntax/SyntaxInput.js
+++ models/Syntax/SyntaxInput.js
@@ -1,10 +1,11 @@
-const {Schema, model} = require("mongoose");
+const {Schema, model, mongoose} = require("mongoose");
+const autoIncrementSyntaxInput = require("mongoose-sequence")(mongoose);
 
 const syntaxInputSchema = new Schema({
     sentence: {type: String, required: true},
     processing: {type: Boolean, default: false},
     processed: {type: Boolean, default: false},
-    processingTime: {type: Date}
-});
+}, {_id: false});
 
+syntaxInputSchema.plugin(autoIncrementSyntaxInput, {id: 'syntax_input_id', inc_field: '_id'});
 module.exports = model("SyntaxInput", syntaxInputSchema);
