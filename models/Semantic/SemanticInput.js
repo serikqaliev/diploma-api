0c4df7310e2a779493e04fc8294d7a8560b52da0
Akzhol <72283384+serikqaliev@users.noreply.github.com>
2022-05-13 22:20:03 +0600

    Update SemanticInput.js

--- models/Semantic/SemanticInput.js
+++ models/Semantic/SemanticInput.js
@@ -1,10 +1,11 @@
-const {Schema, model} = require("mongoose");
+const {Schema, model, mongoose} = require("mongoose");
+const autoIncrementSemantic = require("mongoose-sequence")(mongoose);
 
 const semanticInputSchema = new Schema({
     sentence: {type: String, required: true},
     processing: {type: Boolean, default: false},
     processed: {type: Boolean, default: false},
-    processingTime: {type: Date}
-});
+}, {_id: false});
 
+semanticInputSchema.plugin(autoIncrementSemantic, {id: 'semantic_input_id', inc_field: '_id'});
 module.exports = model("SemanticInput", semanticInputSchema);
